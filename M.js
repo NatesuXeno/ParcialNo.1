802f5a2101169ecca8bb38ec25dd69b6a9e9ee85
NatesuXeno <60716738+NatesuXeno@users.noreply.github.com>
2020-02-10 16:31:52 -0600

    Comparativa de numeros

---
+++ M.js
@@ -0,0 +1,17 @@
+var x=Math.floor(Math.random()*10)+1;
+if(x==10)
+    console.log(x,"A");
+else
+    if(x==9)
+        console.log(x,"B");
+    else
+        if(x==8)
+            console.log(x,"C");
+        else
+            if(x==7)
+                console.log(x,"D");
+            else
+                if(x==6)
+                    console.log(x,"E");
+                else
+                    console.log(x,"F");
